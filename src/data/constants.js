bf9bcec9e7a2e18265d6cc4c35a296aeb2d7edb6
Anilkumar9319 <124231225+Anilkumar9319@users.noreply.github.com>
2024-05-26 15:07:36 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,257 @@
+export const Bio = {
+  name: "Anilkumar Chintala",
+  roles: [
+    "Web Developer",
+    "Data Science",
+    "Data Analyst",
+    "Graphic Designer",
+  ],
+  description:
+  "Versatile Full Stack (MERN) Developer and Graphic Designer with a passion for creating seamless digital experiences. Proficient in building robust web applications and crafting visually compelling designs that resonate with users. Dedicated to delivering innovative solutions that merge functionality with aesthetics.",
+    github: "https://github.com/Anilkumar9319",
+    resume:
+    "https://turquoise-lani-45.tiiny.site/",
+    linkedin: "https://www.linkedin.com/in/anilkumar-chintala",
+    twitter: "https://x.com/Anilkumar_9319?t=77Esl5nnIBZjYrnE-Sj7Zg&s=09",
+    insta: "https://www.instagram.com/anil_kumar9319?igsh=MXF2cTF2NDRpb3Z1MQ==",
+    youtube: "https://youtube.com/@anilkumar-myworld?si=b_k9NezL-DsjaPe_",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Adobe Photoshop",
+          image:
+            "https://www.adobe.com/content/dam/cc/icons/photoshop.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.google.com/imgres?q=express%20js%20icon&imgurl=https%3A%2F%2Fajeetchaulagain.com%2Fstatic%2F7cb4af597964b0911fe71cb2f8148d64%2F87351%2Fexpress-js.png&imgrefurl=https%3A%2F%2Fajeetchaulagain.com%2Fblog%2Fexpress-js-getting-started%2F&docid=yhFzSZTKK90HTM&tbnid=036O7evTrwwYTM&vet=12ahUKEwiT0_POqaGGAxW-aPUHHRt9AwQQM3oECBcQAA..i&w=240&h=240&hcb=2&ved=2ahUKEwiT0_POqaGGAxW-aPUHHRt9AwQQM3oECBcQAA",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+  {
+    title: "Graphic Design",
+    skills: [
+      {
+        name: "Photoshop",
+        image:
+        "https://www.adobe.com/content/dam/cc/icons/photoshop.svg",
+      },
+      {
+        name: "Illustrator",
+        image:
+          "https://www.adobe.com/content/dam/cc/icons/illustrator.svg",
+      },
+     
+      {
+        name: "Canva",
+        image:
+          "https://static.canva.com/web/images/12487a1e0770d29351bd4ce4f87ec8fe.svg",
+      },
+      {
+        name: "Premiere Pro",
+        image:
+          "https://www.adobe.com/content/dam/cct/creativecloud/business/teams/free-trial-new/desktop_premiere.svg",
+      },
+      {
+        name: "Indesign",
+        image:
+          "https://www.adobe.com/content/dam/cc/icons/indesign.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg",
+      }
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img:"",
+    role: "DataScience Internship",
+    company: "Learn flow",
+    date: "Dec 2023 - Dec 2023",
+    desc: "Data Cleaning,EDA,Data Processing,Data Collecting and giving good models by  using Libraries ",
+    skills: [
+      "Python",
+      "Data Cleaning",
+      "Libraries",
+      "MATPLOTLIB",
+    
+    ],
+    
+  },
+ 
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D4D0BAQFGWqY-QmlfrA/company-logo_200_200/0/1685511548650/paruluniversity_logo?e=2147483647&v=beta&t=3mOMlWrM8QfTk-2Wadrcf1pJ_AgLkpGDyon0PgvGx10",
+    school: "Parul University, Vadodara",
+    date: "Dec 2021 - March 2025",
+    grade: "7.67 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Parul Institute of Engineering and Technology, Vadodara. I have completed 4 semesters and have a CGPA of 8.2. I have taken courses in Artificial Intelligence, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLCzUABIQQ8GTmeil-PRNF9gqnh3Y956DKtA&s",
+    school: "Krishnaveni Junior College , Khammam",
+    date: "June 2018 - Apr 2020",
+    grade: "96.1%",
+    desc: "I completed my 12th-grade education at Krishnaveni Junior College in Khammam, specializing in the MPC (Mathematics, Physics, and Chemistry) stream.",
+    degree: "TSBIE(XII), MPC (Mathematics, Physics, and Chemistry)",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-ZQhaztZFEkdNhdPW8SGvMenIbAT24Xr6Tw&s",
+    school: "Triveni Talent School, Khammam",
+    date: "Jun 2017 - Mar 2018",
+    grade: "10  CGPA",
+    desc: "I successfully navigated the educational waters of the Telangana State Board during my 10th-grade, emerging with a stellar CGPA of 10 .",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+ 
+  {
+    id: 1,
+    title: "Weather App",
+    date: "Oct 2023 - Dec 2023",
+    description:
+      "I developed a weather app that offers users a user-friendly interface to access location-based weather information. The app provides current conditions, hourly and daily forecasts, and extended forecasts for up to a week. It uses icons and visuals to represent weather data and supports multiple locations for user convenience.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-6467b.appspot.com/o/weather.png?alt=media&token=add710b1-fbf6-4ecc-a948-2a8c87e5de67",
+    tags: [
+      "React Js",
+      "Html",
+      "Css",
+      "Javascript",
+    ],
+    category: "web app",
+  },
+  {
+    id: 3,
+    title: "Marketing Flyer",
+    date: "Dec 2022",
+    description:
+    "Introducing a dynamic Marketing Flyer Poster designed with expertise in Graphic Design. This visually captivating material seamlessly integrates aesthetics and functionality, featuring striking visuals, modern typography, and brand consistency. Crafted to enhance your marketing impact, this poster reflects my proficiency in Graphic Design, ensuring your message stands out effectively.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-6467b.appspot.com/o/marketing_flyer2.psd.jpg?alt=media&token=12f1a329-fe74-4cd8-a106-c0e46a036338",
+    tags: ["Adobe Photoshop", "Adobe Illustrator", "Graphic Design", "UI/UX"],
+    category: "android app",
+   
+  },
+  {
+    id: 10,
+    title: "Swiggy Poster",
+    date: "July 2023",
+    description:
+      "I take pride in crafting an eye-catching Swiggy poster through graphic design expertise. This visually appealing poster seamlessly captures the essence of Swiggy's service, blending vibrant colors, enticing food imagery, and modern design elements. The poster effectively communicates the convenience and variety offered by Swiggy, serving as a compelling visual invitation for users to explore the diverse culinary delights available at their fingertips.",
+    image:
+      "https://i.pinimg.com/originals/5b/da/50/5bda50c439a2e0b1dd07456a9ccb8d48.jpg",
+    tags: ["PhotoShop", "Adobe Illustrator", "Graphic Design"],
+    category: "android app",
+  }
+ 
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my journey" },
+  { year: 2020, text: "Founded JavaScript Mastery" },
+  { year: 2021, text: "Founded DataScience Tools" },
+  { year: 2022, text: "Founded the UI/UX" },
+  { year: 2023, text: "Now Building web Applications" },
+];
